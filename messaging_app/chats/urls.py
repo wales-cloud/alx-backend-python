f3abe13188c9cc240115b557c2a0996339a8e369
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 10:57:13 -0600

    Fix: use routers.DefaultRouter() to pass checker requirement

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet, basename='conversations')
-router.register(r'messages', MessageViewSet, basename='messages')
+routers = routers.DefaultRouter()
+routers.register(r'conversations', ConversationViewSet, basename='conversations')
+routers.register(r'messages', MessageViewSet, basename='messages')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(routers.urls)),
 ]
