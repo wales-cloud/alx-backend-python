d74001c840075f23ee447a2980df44060d1c3377
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 09:04:13 -0600

    Add test for KeyError in access_nested_map with parameterized inputs

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,17 +4,29 @@ import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Test class for access_nested_map"""
 
     @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ("simple", {"a": 1}, ("a",), 1),
+        ("nested_first_level", {"a": {"b": 2}}, ("a",), {"b": 2}),
+        ("nested_second_level", {"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map returns expected value"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test access_nested_map raises KeyError with expected message"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), repr(path[len(context.exception.args[0])]))
+
 
 if __name__ == "__main__":
     unittest.main()
