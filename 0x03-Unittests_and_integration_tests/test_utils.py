c30f8ec458275fdaed94349da9d607692c40cc52
Adewale Salami <wales.abbe@gmail.com>
2025-06-01 09:07:05 -0600

    Fix: correct KeyError test in access_nested_map using parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,9 +9,9 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test class for access_nested_map"""
 
     @parameterized.expand([
-        ("simple", {"a": 1}, ("a",), 1),
-        ("nested_first_level", {"a": {"b": 2}}, ("a",), {"b": 2}),
-        ("nested_second_level", {"a": {"b": 2}}, ("a", "b"), 2),
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map returns expected value"""
@@ -26,7 +26,3 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), repr(path[len(context.exception.args[0])]))
-
-
-if __name__ == "__main__":
-    unittest.main()
