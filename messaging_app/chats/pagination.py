8beeffbb8fd7d2db3741b8ff31a2ffbcb27663c2
Adewale Salami <wales.abbe@gmail.com>
2025-06-07 13:32:59 -0600

    Fix: Add paginator.count to MessagePagination response for checker

--- messaging_app/chats/pagination.py
+++ messaging_app/chats/pagination.py
@@ -1,6 +1,21 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class MessagePagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            "page": {
+                "paginator": {
+                    "count": self.page.paginator.count,
+                    "num_pages": self.page.paginator.num_pages,
+                },
+                "current": self.page.number,
+                "has_next": self.page.has_next(),
+                "has_previous": self.page.has_previous(),
+            },
+            "results": data
+        })
