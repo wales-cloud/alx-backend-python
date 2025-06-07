839487d1b50bbe4e4358d56c9a4e5ec40f54d3ca
Adewale Salami <wales.abbe@gmail.com>
2025-06-07 12:25:25 -0600

    Fix: Add Basic and Session authentication for checker compliance

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -129,11 +129,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
 from datetime import timedelta
 
 SIMPLE_JWT = {
